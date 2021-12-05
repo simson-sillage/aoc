4dbb945b4534a4a62102c21a088ee529f5522d1f
Simon Schricker <s.schricker@sillage.at>
2021-12-05 21:11:32 +0100

    2021 day5: remove unnecessary main function

--- 2021/day5/part1.py
+++ 2021/day5/part1.py
@@ -1,48 +1,44 @@
 import sys
 
 
-def main():
-    if len(sys.argv) != 2:
-        sys.exit("error: provide filename of input as argument")
+if len(sys.argv) != 2:
+    sys.exit("error: provide filename of input as argument")
 
-    with open(sys.argv[1], 'r') as file:
-        inputs = file.read().strip().split('\n')
+with open(sys.argv[1], 'r') as file:
+    inputs = file.read().strip().split('\n')
 
-    lines = []
-    for input in inputs:
-        start_cords, _, end_cords = input.split(' ')
-        x0, y0 = start_cords.split(',')
-        x1, y1 = end_cords.split(',')
-        x0 = int(x0)
-        y0 = int(y0)
-        x1 = int(x1)
-        y1 = int(y1)
+lines = []
+for input in inputs:
+    start_cords, _, end_cords = input.split(' ')
+    x0, y0 = start_cords.split(',')
+    x1, y1 = end_cords.split(',')
+    x0 = int(x0)
+    y0 = int(y0)
+    x1 = int(x1)
+    y1 = int(y1)
 
-        # consider only horizontal or vertical lines
-        if not (x0 == x1 or y0 == y1):
-            continue
+    # consider only horizontal or vertical lines
+    if not (x0 == x1 or y0 == y1):
+        continue
 
-        line = set()
-        for i in range(0, abs(x1-x0)+1, 1):
-            point = (min(x0, x1) + i, y0)
-            line.add(point)
-        for i in range(0, abs(y1-y0)+1, 1):
-            point = (x0, min(y0, y1) + i)
-            line.add(point)
+    line = set()
+    for i in range(0, abs(x1-x0)+1, 1):
+        point = (min(x0, x1) + i, y0)
+        line.add(point)
+    for i in range(0, abs(y1-y0)+1, 1):
+        point = (x0, min(y0, y1) + i)
+        line.add(point)
 
-        lines.append(line)
+    lines.append(line)
 
-    count = {}
-    for line in lines:
-        for point in line:
-            count[point] = count.get(point, 0) + 1
+count = {}
+for line in lines:
+    for point in line:
+        count[point] = count.get(point, 0) + 1
 
-    count_overlapping_points = 0
-    for point, counter in count.items():
-        if counter >= 2:
-            count_overlapping_points += 1
+count_overlapping_points = 0
+for point, counter in count.items():
+    if counter >= 2:
+        count_overlapping_points += 1
 
-    print(f"count of points with overlap: {count_overlapping_points}")
-
-
-main()
+print(f"count of points with overlap: {count_overlapping_points}")
--- 2021/day5/part2.py
+++ 2021/day5/part2.py
@@ -1,62 +1,58 @@
 import sys
 
 
-def main():
-    if len(sys.argv) != 2:
-        sys.exit("error: provide filename of input as argument")
+if len(sys.argv) != 2:
+    sys.exit("error: provide filename of input as argument")
 
-    with open(sys.argv[1], 'r') as file:
-        inputs = file.read().strip().split('\n')
+with open(sys.argv[1], 'r') as file:
+    inputs = file.read().strip().split('\n')
 
-    lines = []
-    for input in inputs:
-        start_cords, _, end_cords = input.split(' ')
-        x0, y0 = start_cords.split(',')
-        x1, y1 = end_cords.split(',')
-        x0 = int(x0)
-        y0 = int(y0)
-        x1 = int(x1)
-        y1 = int(y1)
+lines = []
+for input in inputs:
+    start_cords, _, end_cords = input.split(' ')
+    x0, y0 = start_cords.split(',')
+    x1, y1 = end_cords.split(',')
+    x0 = int(x0)
+    y0 = int(y0)
+    x1 = int(x1)
+    y1 = int(y1)
 
-        line = set()
+    line = set()
 
-        # horizontal or vertical lines
-        if x0 == x1 or y0 == y1:
-            for i in range(0, abs(x1-x0)+1, 1):
-                point = (min(x0, x1) + i, y0)
-                line.add(point)
-            for i in range(0, abs(y1-y0)+1, 1):
-                point = (x0, min(y0, y1) + i)
-                line.add(point)
-        # diagonal line
-        else:
-            x_change = -1 if x0 > x1 else 1
-            y_change = -1 if y0 > y1 else 1
+    # horizontal or vertical lines
+    if x0 == x1 or y0 == y1:
+        for i in range(0, abs(x1-x0)+1, 1):
+            point = (min(x0, x1) + i, y0)
+            line.add(point)
+        for i in range(0, abs(y1-y0)+1, 1):
+            point = (x0, min(y0, y1) + i)
+            line.add(point)
+    # diagonal line
+    else:
+        x_change = -1 if x0 > x1 else 1
+        y_change = -1 if y0 > y1 else 1
 
-            xi = x0
-            yi = y0
-            while xi != x1:
-                point = (xi, yi)
-                line.add(point)
-                xi += x_change
-                yi += y_change
+        xi = x0
+        yi = y0
+        while xi != x1:
+            point = (xi, yi)
+            line.add(point)
+            xi += x_change
+            yi += y_change
 
-            # add the last point
-            line.add((x1, y1))
+        # add the last point
+        line.add((x1, y1))
 
-        lines.append(line)
+    lines.append(line)
 
-    count = {}
-    for line in lines:
-        for point in line:
-            count[point] = count.get(point, 0) + 1
+count = {}
+for line in lines:
+    for point in line:
+        count[point] = count.get(point, 0) + 1
 
-    count_overlapping_points = 0
-    for point, counter in count.items():
-        if counter >= 2:
-            count_overlapping_points += 1
+count_overlapping_points = 0
+for point, counter in count.items():
+    if counter >= 2:
+        count_overlapping_points += 1
 
-    print(f"count of points with overlap: {count_overlapping_points}")
-
-
-main()
+print(f"count of points with overlap: {count_overlapping_points}")
