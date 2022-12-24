7d894545c8b75d15a35ab9a66f7e247b2ad065a3
Simon Schricker <s.schricker@sillage.at>
2022-12-24 15:52:18 +0100

    add: 2022 day 1, part 2

--- 2022/day1/part2.ts
+++ 2022/day1/part2.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import * as fs from 'fs';
+import { openStdin } from 'process';
 
 if (process.argv.length != 3) {
     console.log(`usage: ${process.argv[0]} <filename>`);
@@ -10,15 +11,20 @@ const filename = process.argv[2];
 const input = fs.readFileSync(filename, 'utf8');
 
 const elves = input.split(/\r?\n\r?\n/).filter(e => e);
-var biggest = -1;
+let summedCaloriesPerElf: number[] = [];
 
 elves.forEach(elf => {
     const calories = elf.split(/\r?\n/).filter(e => e);
     const parsedCalories = calories.map(e => parseInt(e));
     const sum = parsedCalories.reduce((a, b) => a + b, 0);
-    if (sum > biggest) {
-        biggest = sum;
-    }
+    summedCaloriesPerElf.push(sum);
 })
+summedCaloriesPerElf.sort((a, b) => b - a);
 
-console.log("biggest sum of calories on a elf:", biggest);
+if (summedCaloriesPerElf.length < 3) {
+    console.log("error: less than 3 elves in input.");
+    process.exit(2);
+}
+
+const answer = summedCaloriesPerElf[0] + summedCaloriesPerElf[1] + summedCaloriesPerElf[2];
+console.log("sum of the 3 richest elves:", answer);
